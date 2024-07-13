b87aa7576c853233aed693d187e89553922a67c2
Ajay B <39263644+ajaybenedict@users.noreply.github.com>
2024-07-13 17:00:32 +0530

    Update Pagination.js

--- src/components/pageProps/shopPage/Pagination.js
+++ src/components/pageProps/shopPage/Pagination.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactPaginate from "react-paginate";
 import Product from "../../home/Products/Product";
 import { useSelector } from "react-redux";
 import { paginationItems } from "../../../constants";
@@ -24,16 +23,9 @@ function Items({ currentItems, selectedBrands, selectedCategories }) {
     <>
       {filteredItems.map((item) => (
         <div key={item._id} className="w-full">
-          <Product
-            _id={item._id}
+          <Product    
             img={item.img}
-            productName={item.productName}
-            price={item.price}
-            color={item.color}
-            badge={item.badge}
-            des={item.des}
-            pdf={item.pdf}
-            ficheTech={item.ficheTech}
+            productName={item.productName}       
           />
         </div>
       ))}
@@ -43,7 +35,6 @@ function Items({ currentItems, selectedBrands, selectedCategories }) {
 
 const Pagination = ({ itemsPerPage }) => {
   const [itemOffset, setItemOffset] = useState(0);
-  const [itemStart, setItemStart] = useState(1);
 
   const endOffset = itemOffset + itemsPerPage;
   const currentItems = items.slice(itemOffset, endOffset);
@@ -53,15 +44,7 @@ const Pagination = ({ itemsPerPage }) => {
   const selectedCategories = useSelector(
     (state) => state.orebiReducer.checkedCategorys
   );
-  const pageCount = Math.ceil(items.length / itemsPerPage);
-
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    const newStart = newOffset + 1; // Adjust the start index
-
-    setItemOffset(newOffset);
-    setItemStart(newStart);
-  };
+ 
 
   return (
     <div>
@@ -71,27 +54,7 @@ const Pagination = ({ itemsPerPage }) => {
           selectedBrands={selectedBrands}
           selectedCategories={selectedCategories}
         />{" "}
-      </div>
-      <div className="flex flex-col mdl:flex-row justify-center mdl:justify-between items-center">
-        <ReactPaginate
-          nextLabel=""
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={3}
-          marginPagesDisplayed={2}
-          pageCount={pageCount}
-          previousLabel=""
-          pageLinkClassName="w-9 h-9 border-[1px] border-lightColor hover:border-gray-500 duration-300 flex justify-center items-center"
-          pageClassName="mr-6"
-          containerClassName="flex text-base font-semibold font-titleFont py-10"
-          activeClassName="bg-black text-white"
-        />
-
-        <p className="text-base font-normal text-lightText">
-          Products from {itemStart} to {Math.min(endOffset, items.length)} of{" "}
-          {items.length}
-        </p>
-        <button onClick={() => console.log(selectedBrands)}> test</button>
-      </div>
+      </div>      
     </div>
   );
 };
