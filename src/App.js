301b195980f6bc47190dfdc7160b335643260fc8
Ajay B <39263644+ajaybenedict@users.noreply.github.com>
2024-07-13 16:57:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,49 +4,13 @@ import {
   Outlet,
   createRoutesFromElements,
   Route,
-  ScrollRestoration,
 } from "react-router-dom";
-import Footer from "./components/home/Footer/Footer";
-import FooterBottom from "./components/home/Footer/FooterBottom";
-import Header from "./components/home/Header/Header";
-import HeaderBottom from "./components/home/Header/HeaderBottom";
-import SpecialCase from "./components/SpecialCase/SpecialCase";
-import About from "./pages/About/About";
-import SignIn from "./pages/Account/SignIn";
-import SignUp from "./pages/Account/SignUp";
-import Cart from "./pages/Cart/Cart";
-import Contact from "./pages/Contact/Contact";
-import Home from "./pages/Home/Home";
-import Journal from "./pages/Journal/Journal";
-import Offer from "./pages/Offer/Offer";
-import Payment from "./pages/payment/Payment";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import Shop from "./pages/Shop/Shop";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => {
   return (
-    <div>
-      <ToastContainer
-        position="top-right"
-        autoClose={1000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      <Header />
-      <HeaderBottom />
-      <SpecialCase />
-      <ScrollRestoration />
-      <Outlet />
-      <Footer />
-      <FooterBottom />
+    <div>   
+      <Outlet />   
     </div>
   );
 };
@@ -54,27 +18,16 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Layout />}>
-        {/* ==================== Header Navlink Start here =================== */}
-        <Route index element={<Home />}></Route>
+        <Route index element={<Shop />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/journal" element={<Journal />}></Route>
-        {/* ==================== Header Navlink End here ===================== */}
-        <Route path="/category/:category" element={<Offer />}></Route>
-        <Route path="/product/:_id" element={<ProductDetails />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/paymentgateway" element={<Payment />}></Route>
       </Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-      <Route path="/signin" element={<SignIn />}></Route>
     </Route>
   )
 );
 
 function App() {
   return (
-    <div className="font-bodyFont">
+    <div className="bg-white dark:bg-gray-900 font-bodyFont">
       <RouterProvider router={router} />
     </div>
   );
