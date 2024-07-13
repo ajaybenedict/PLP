6ab2ae28942eb1771ba7b94ecd043e3775acad2c
Ajay B <39263644+ajaybenedict@users.noreply.github.com>
2024-07-13 17:01:33 +0530

    Update Shop.js

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,29 +1,17 @@
 import React, { useState } from "react";
-import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 import Pagination from "../../components/pageProps/shopPage/Pagination";
-import ProductBanner from "../../components/pageProps/shopPage/ProductBanner";
-import ShopSideNav from "../../components/pageProps/shopPage/ShopSideNav";
+
 
 const Shop = () => {
   const [itemsPerPage, setItemsPerPage] = useState(48);
-  const itemsPerPageFromBanner = (itemsPerPage) => {
-    setItemsPerPage(itemsPerPage);
-  };
 
   return (
-    <div className="max-w-container mx-auto px-4">
-      <Breadcrumbs title="Products" />
-      {/* ================= Products Start here =================== */}
+    <div className="dark:bg-slate-800 max-w-container mx-auto px-4">
       <div className="w-full h-full flex pb-20 gap-10">
-        <div className="w-[20%] lgl:w-[25%] hidden mdl:inline-flex h-full">
-          <ShopSideNav />
-        </div>
-        <div className="w-full mdl:w-[80%] lgl:w-[75%] h-full flex flex-col gap-10">
-          <ProductBanner itemsPerPageFromBanner={itemsPerPageFromBanner} />
+        <div className="w-full mdl:w-[100%] lgl:w-[100%] h-full flex flex-col gap-10">
           <Pagination itemsPerPage={itemsPerPage} />
         </div>
       </div>
-      {/* ================= Products End here ===================== */}
     </div>
   );
 };
