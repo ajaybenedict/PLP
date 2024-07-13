efdd074432025e47e1c1a449aa362e252fb7257f
Ajay B <39263644+ajaybenedict@users.noreply.github.com>
2024-07-13 16:57:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PersistGate } from "redux-persist/integration/react";
-import "slick-carousel/slick/slick.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import "./index.css";
